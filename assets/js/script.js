fa97e47cbda1252a6c59419ded5b912d556019b6
rwspatin <rwspatin@gmail.com>
2017-09-24 21:39:35 -0300

    Envio 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,12 +18,42 @@ function ValidarCode2(){
 	var flag = true;
 
 	if(variavel.value == "Silencio" || variavel.value == "silencio"){
-		window.location.href = "p2.html#work";
+		window.location.href = "p3.html#work";
 		return true;
 	}else{
 
 		flag=false;
 		alert("Errou feio, Errou feio, Errou rude");
+		return false;
+	}
+}
+
+function ValidarCode3(){
+	var variavel = document.getElementById("juquinha");
+	var flag = true;
+
+	if(variavel.value == "Os dias da semana" || variavel.value == "os dias da semana" || variavel.value="segunda, terça, quarta, quinta e sexta"){
+		window.location.href = "p4.html#work";
+		return true;
+	}else{
+
+		flag=false;
+		alert("U have Fail");
+		return false;
+	}
+}
+
+function ValidarCode3(){
+	var variavel = document.getElementById("juquinha");
+	var flag = true;
+
+	if(variavel.value == "Um" || variavel.value == "um" || variavel.value="1"){
+		window.location.href = "p4.html#work";
+		return true;
+	}else{
+
+		flag=false;
+		alert("Errou");
 		return false;
 	}
 }
